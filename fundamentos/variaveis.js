30ba83af05bcaff220d9780048b2ddb74451994a
Thiago Nascimento <thiagohfn2005@gmail.com>
2024-09-13 21:36:01 -0300

    variaveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * Estudo de constante e variavel
+ * @author Thiago Nascimento
+*/
+
+console.clear()
+
+//variaveis
+let nome 
+let idade 
+let vip
+
+nome = "Thiago Nacimento"
+console.log(typeof(nome))
+console.log(nome)
+
+idade= "19"
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true 
+console.log(typerof(vip))
+console.log(log)
