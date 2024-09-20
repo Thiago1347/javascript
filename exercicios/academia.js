d3d1b1fd071bbb4ceb8f51beea6a79ee5d423588
Thiago Nascimento <thiagohfn2005@gmail.com>
2024-09-19 22:15:57 -0300

    criaçao da pasta exercicios

---
+++ exercicios/academia.js
@@ -0,0 +1,28 @@
+/**
+ * Ficha de um aluno de academia 
+ * @author Thiago Nascimento
+ */
+
+//variaveis 
+let nome 
+let idade
+let peso
+let altura
+let vip 
+
+// entrada de dados
+console.clear()
+nome = "Thiago Nascimento"
+idade = 19
+peso = 65
+altura = 1.72
+vip = true
+
+// saida
+console.log("ficha do aluno")
+console.log("_______________________")
+console.log(`nome:${nome}`)
+console.log(`idade: ${idade}`)
+console.log(`peso: ${peso}`)
+console.log(`altura: ${altura}`)
+console.log(`Vip: ${vip}`)
