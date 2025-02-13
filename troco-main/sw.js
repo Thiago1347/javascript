552e270dccbde3e5a7a3d80e59fb496c94dd5274
Thiago Nascimento <thiagoh378@gmail.com>
2025-02-13 20:02:02 -0300

    app troco

---
+++ troco-main/sw.js
@@ -0,0 +1,48 @@
+// sw.js
+
+const cacheName = 'troco-cache-v1';
+const filesToCache = [
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon-192x192.png',
+  './icon-512x512.png'
+];
+
+// Instalar o Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+// Ativar o Service Worker
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [cacheName];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requisições e servir do cache, se possível
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
